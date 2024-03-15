d6dd4c1342c43f2a2d1f81a90ec18cf5b57462c2
RajuShivarama <rajushivaram97@gmail.com>
2024-03-15 13:24:33 +0530

    Took 3h req-B1 added

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -5,6 +5,17 @@ class AppsController < ApplicationController
     @apps = filter_apps
   end
 
+  def by_names
+    if params[:names].present?
+      @apps = App.includes(:users).where(name: params[:names])
+      render json: @apps, include: :users
+    else
+      render json: { error: 'No app names provided' }, status: :bad_request
+    end
+
+    # RAQ SQL
+    # SELECT "apps".* FROM "apps" WHERE "apps"."name" IN ('App Name 1', 'App Name 2');
+
   private
 
     # Only allow a list of trusted parameters through.
