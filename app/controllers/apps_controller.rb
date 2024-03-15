a8137bf7a87d7e7ddbfd28ddb301b9cfd6290642
RajuShivarama <rajushivaram97@gmail.com>
2024-03-15 12:58:40 +0530

    Took 2h -  A1 requirement

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -12,4 +12,18 @@ class AppsController < ApplicationController
       params.permit(:approved, :published, :kind, :price_tier)
     end
 
+    # Filter apps based on query parameters
+    def filter_apps
+      apps = App.all
+      apps = apps.where(approved: params[:approved]) if params[:approved].present?
+      apps = apps.where(published: params[:published]) if params[:published].present?
+      apps = apps.where(kind: params[:kind]) if params[:kind].present?
+      apps = apps.where(price_tier: params[:price_tier]) if params[:price_tier].present?
+      apps
+    end
+
+    # SQL for above ORM
+    # For example, if a user wants to find approved, published games within price tier 1, the SQL might look like this:
+    # SELECT * FROM apps WHERE approved = true AND published = true AND kind = 'game' AND price_tier = 1;
+
 end
